9004dae93639906ce5f6781fb181f2e9c844c8f7
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-29 09:26:13 +0200

    removed useless morphology

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,6 @@
 from src.image_ops import show_img
-from src.image_ops.morphology import morph_open, morph_close
-from src.mask_color import mask_color, mask_color_single
+from src.mask_color import mask_color_single
 
 
 def test_color_masking(img, color, tolerance):
-    morphed_img = morph_close(img, kernel_size=13)
-    show_img('result', mask_color_single(morphed_img, color, tolerance))
+    show_img('result', mask_color_single(img, color, tolerance))
