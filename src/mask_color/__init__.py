239814fce6cd6c4e323551c6830b5fb19d9e7bcb
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-31 10:17:11 +0200

    extracted function

--- src/mask_color/__init__.py
+++ src/mask_color/__init__.py
@@ -4,5 +4,9 @@ import math
 def mask_color_point(img, color_bgr, tolerance):
     for r, row in enumerate(img):
         for p, pix in enumerate(row):
-            img[r][p] = [0, 0, 0] if math.dist(pix, color_bgr) > tolerance else pix
+            img[r][p] = get_pixel_color(color_bgr, pix, tolerance)
     return img
+
+
+def get_pixel_color(color_bgr, pix, tolerance):
+    return pix if math.dist(pix, color_bgr) < tolerance else [0, 0, 0]
