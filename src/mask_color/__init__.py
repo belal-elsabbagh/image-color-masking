ee8eecf93f723ab4f68d0b7845f8321bbb29a26b
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-31 09:53:39 +0200

    small opt

--- src/mask_color/__init__.py
+++ src/mask_color/__init__.py
@@ -4,6 +4,5 @@ import math
 def mask_color_point(img, color_bgr, tolerance):
     for r, row in enumerate(img):
         for p, pix in enumerate(row):
-            if math.dist(pix, color_bgr) > tolerance:
-                img[r][p] = [0, 0, 0]
+            img[r][p] = [0, 0, 0] if math.dist(pix, color_bgr) > tolerance else pix
     return img
